ded41de07a1b94b33bb4a1e667d7c4e5d439b078
king1991wbs <king1991wbs@gmail.com>
2014-04-12 14:17:07 +0800

    changes

---
+++ README.md
@@ -0,0 +1,3 @@
+this branch is the answer to leetcode writen by me using c++...
+some of the code may referred the source in leetcode.com
+
---
+++ reversebit.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: reversebit.cpp
+	> Author: wilson_91
+	> Mail: wilson_91@foxmail.com 
+	> Created Time: Tue 01 Apr 2014 10:35:33 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    return 
+}
