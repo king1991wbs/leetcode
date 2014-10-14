a3b42d58b7e8aade0bebb4cb9da6d33df69450d8
wilson <king1991wbs@gmail.com>
2014-10-14 13:00:54 +0800

    python version of post order binary tree

---
+++ postorderbianry.py
@@ -0,0 +1,20 @@
+# Definition for a  binary tree node
+ class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def __init__(self):
+        self.post_sequence = []
+    def postorderTraversal(self, root):
+        if root == None:
+            return self.post_sequence
+        self.postorderTraversal(root.left)
+        self.postorderTraversal(root.right)
+        #print root.val
+        self.post_sequence.append(root.val)
+        return self.post_sequence
