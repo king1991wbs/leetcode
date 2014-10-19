360af7ee2c917eb9b822b5b259152e4c06d82fe5
king1991wbs <king1991wbs@gmail.com>
2014-10-19 11:48:58 +0800

    递归版本

---
+++ wordbreak_recur.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a boolean
+    def wordBreak(self, s, dict):
+    	if s in dict:
+    		return True
+        for i in range(1, len(s)):
+        	word = s[:i]
+        	if word in dict:
+        		if self.wordBreak(s[i:], dict) == True:
+        			return True
+        		else:
+        			continue
+        return False
