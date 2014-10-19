39b15e299fc24a99c598b8cd89cd278337e5798d
wilson <king1991wbs@gmail.com>
2014-10-19 19:43:01 +0800

    all right but time limeted

--- wordbreak_recur.py
+++ wordbreak_recur.py
@@ -3,15 +3,15 @@ class Solution:
     # @param dict, a set of string
     # @return a boolean
     def wordBreak(self, s, dict):
-    	if s in dict:
-    		return True
+        if s in dict:
+            return True
         for i in range(1, len(s)):
-        	word = s[:i]
-        	if word in dict:
-        		if self.wordBreak(s[i:], dict) == True:
-        			return True
-        		else:
-        			continue
+            word = s[:i]
+            if word in dict:
+                if self.wordBreak(s[i:], dict) == True:
+                    return True
+                else:
+                    continue
         return False
 sol = Solution()
 sentence = "fohhemkkaecojceoaejkkoedkofhmohkcjmkggcmnami"
